78958f52859a99facc069c94055774e7e402a508
Igor Savinov <igorsavinov@192.168.1.29>
2026-01-13 18:04:30 +0800

    Add CI with Cypress

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -2,8 +2,18 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "http://localhost:3000",
+
+    specPattern: "cypress/e2e/**/*.cy.{js,ts}",
+
+    supportFile: "cypress/support/e2e.js",
+
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 60000,
+
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // node events (пока не нужны)
+      return config;
     },
   },
 });
