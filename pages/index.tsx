e2a6b9fb165eb3fb8f70acc046932279f832ddbe
Igor Savinov <ikix46@gmail.com>
2026-01-18 23:48:42 +0800

    fix: add root index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,27 @@
-import Head from "next/head"
-import Layout from "../components/Layout"
-import HomeHero from "../components/Home/HomeHero"
-import HomeFeatures from "../components/Home/HomeFeatures"
-import HomeCourses from "../components/Home/HomeCourses"
-import { progressService } from "../machines/progressService"
+import Link from "next/link"
 import { fetchCourses } from "../lib/fetch-courses"
 
-export default function Home({ content, courses }) {
+export default function Home({ courses }) {
   return (
-    <Layout
-      content={content}
-      courses={courses}
-      progressService={progressService}
-    >
-      <Head>
-        <title>Testing Next.js Applications with Cypress</title>
-        <meta
-          name="description"
-          content="Learn from top industry experts and level-up your testing knowledge - for free."
-        />
-      </Head>
-
-      <HomeHero />
-      <HomeFeatures />
-      <HomeCourses
-        courses={courses}
-        content={content}
-        progressService={progressService}
-      />
-    </Layout>
+    <div style={{ padding: 24 }}>
+      <h1>Courses</h1>
+      <ul>
+        {courses.map((course) => (
+          <li key={course}>
+            <Link href={`/${course}`}>{course}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
-export async function getStaticProps({ params }) {
+export async function getStaticProps() {
   const coursesJson = await fetchCourses()
-  const courses = Object.keys(coursesJson)
+
   return {
     props: {
-      content: coursesJson,
-      courses,
+      courses: Object.keys(coursesJson),
     },
   }
 }
