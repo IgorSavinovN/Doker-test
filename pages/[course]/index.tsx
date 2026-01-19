07146a56957c246d3450af8d839b17d4a4446018
Igor Savinov <ikix46@gmail.com>
2026-01-19 14:09:45 +0800

    restore home page as original first course

--- pages/[course]/index.tsx
+++ pages/[course]/index.tsx
@@ -1,75 +1,47 @@
 import Head from "next/head"
-import Layout from "../../components/Layout"
-import CourseHero from "../../components/Course/CourseHero"
-import CourseContent from "../../components/Course/CourseContent"
-import { progressService } from "../../machines/progressService"
-import { fetchCourses } from "../../lib/fetch-courses"
+import Layout from "../components/Layout"
+import CourseHero from "../components/Course/CourseHero"
+import CourseContent from "../components/Course/CourseContent"
+import { progressService } from "../machines/progressService"
+import { fetchCourses } from "../lib/fetch-courses"
 
-export default function SectionPage({
-  title,
-  lessons,
-  description,
-  learnFeatures,
-  content,
-  courses,
-  course,
-}) {
+export default function HomePage({ courses, content }) {
+  // Берём первый курс для отображения на главной
+  const firstCourse = Object.keys(content)[0]
+  const { title, lessons, description, learnFeatures } = content[firstCourse]
+
   return (
-    <Layout
-      content={content}
-      courses={courses}
-      progressService={progressService}
-    >
+    <Layout content={content} courses={courses} progressService={progressService}>
       <Head>
-        <title>{title} | Testing Next.js Applications with Cypress</title>
+        <title>{title} | Cypress Real World Testing</title>
         <meta name="description" content={description} />
       </Head>
 
       <CourseHero
         title={title}
         description={description}
-        image={content[course].image}
+        image={content[firstCourse].image}
       />
+
       <CourseContent
         title={title}
         lessons={lessons}
         learnFeatures={learnFeatures}
         progressService={progressService}
-        course={course}
+        course={firstCourse}
       />
     </Layout>
   )
 }
 
-export async function getStaticProps({ params }) {
+export async function getStaticProps() {
   const coursesJson = await fetchCourses()
-  const { title, lessons, description, learnFeatures } =
-    coursesJson[params.course]
   const courses = Object.keys(coursesJson)
 
   return {
     props: {
-      lessons,
-      title,
-      description,
-      learnFeatures,
-      content: coursesJson,
       courses,
-      course: params.course,
+      content: coursesJson,
     },
-  }
-}
-
-export async function getStaticPaths() {
-  const coursesJson = await fetchCourses()
-  const courses = Object.keys(coursesJson)
-  const paths = courses.map((course) => {
-    const { title, lessons } = coursesJson[course]
-    return { params: { course, lessons, title } }
-  })
-
-  return {
-    paths,
-    fallback: false,
   }
 }
